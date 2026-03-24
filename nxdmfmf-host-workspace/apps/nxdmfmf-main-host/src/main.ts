2e8cf71e5a9aec7c4d76bd65ca4ae9f7836be53e
László Kővári <laszlo.kovary@gmail.com>
2026-03-24 01:21:02 +0100

    build and deploy to github pages capability

--- nxdmfmf-host-workspace/apps/nxdmfmf-main-host/src/main.ts
+++ nxdmfmf-host-workspace/apps/nxdmfmf-main-host/src/main.ts
@@ -1,6 +1,6 @@
 import { registerRemotes } from '@module-federation/enhanced/runtime';
 
-fetch('/module-federation.manifest.json')
+fetch('module-federation.manifest.json')
   .then((res) => res.json())
   .then((remotes: Record<string, string>) =>
     Object.entries(remotes).map(([name, entry]) => ({ name, entry }))
